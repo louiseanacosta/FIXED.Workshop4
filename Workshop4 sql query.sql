1b1fcac90a1d1d71eee9b319d969f1a7ff450966
DongMing <hdmdhr@gmail.com>
2019-04-05 10:29:04 -0600

    Update Workshop4 sql query.sql

--- Workshop4 sql query.sql
+++ Workshop4 sql query.sql
@@ -8,17 +8,19 @@ select * from Products
 SELECT q.ProductSupplierId, p.ProdName, s.SupName
 FROM Products p, Products_Suppliers q, Suppliers s, Packages t, Packages_Products_Suppliers u
 WHERE p.ProductId = q.ProductId and s.SupplierId = q.SupplierId and q.ProductSupplierId = u.ProductSupplierId and t.PackageId=u.PackageId and t.PackageId=2
+
+
+
+--- SELECT PRODUCTS THAT ARE NOT INCLUDED IN PACKAGES
 SELECT q.ProductSupplierId, p.ProdName, s.SupName
 FROM Products p, Products_Suppliers q, Suppliers s
 WHERE p.ProductId = q.ProductId and s.SupplierId = q.SupplierId and ProductSupplierId NOT IN
 (SELECT ProductSupplierId FROM Packages_Products_Suppliers)
 ORDER BY ProductSupplierId
 
---- SELECT PRODUCTS THAT ARE NOT INCLUDED IN PACKAGES
-
 
 -- SELECT ALL PRODUCTS (BOTH INCLUDED AND NOT INCLUDED IN PACKAGES
-SELECT q.ProductSupplierId, p.ProdName, s.SupName
-FROM Products p, Products_Suppliers q, Suppliers s
-WHERE p.ProductId = q.ProductId and s.SupplierId = q.SupplierId 
+SELECT ps.ProductSupplierId, p.ProdName, s.SupName
+FROM Products p, Products_Suppliers ps, Suppliers s
+WHERE p.ProductId = ps.ProductId and s.SupplierId = ps.SupplierId 
 
